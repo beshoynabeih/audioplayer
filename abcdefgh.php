d1422efd83b896291d1c9fe9eac5d7b62f97ada6
Beshoy Nabeih <c-82403016@s-mu.edu.eg>
2017-08-18 18:16:24 +0300

    Update abcdefgh.php

--- abcdefgh.php
+++ abcdefgh.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors",1);
-if(file_exists("log.txt")){
-$h  = file_get_contents("log.txt");
+if(file_exists("logs.txt")){
+$h  = file_get_contents("logs.txt");
 echo $h;}
    else
    echo "..";
